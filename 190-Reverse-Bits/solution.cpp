86f771b615ce8f0314f090e83f7b61fe37165af7
xuchaoUCAS <xuchao0813@gmail.com>
2016-05-12 22:10:06 +0800

    [190. Reverse Bits][Accepted]committed by LeetCode xuchao@ucas

--- 190-Reverse-Bits/solution.cpp
+++ 190-Reverse-Bits/solution.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        stack<int> bit;
-        int result = 0;
+        uint32_t result = 0;
         for(int i = 0;i < 32; ++i){
-            bit.push(n & 1);
+            result <<= 1;
+            result |= (n & 1);
             n >>= 1;
-        }
-        for(int i = 0;i < 32; ++i){
-            result |= (bit.top() << i);
-            bit.pop();
         }
         return result;
     }
