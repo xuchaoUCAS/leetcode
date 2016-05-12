35736982b0beb62f25e708987a3d9561c1b9160f
xuchaoUCAS <xuchao0813@gmail.com>
2016-05-12 22:38:59 +0800

    [190. Reverse Bits][Accepted]committed by LeetCode xuchao@ucas

--- 190-Reverse-Bits/solution.cpp
+++ 190-Reverse-Bits/solution.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0;i < 32; ++i){
             result <<= 1;
             result |= (n & 1);
-            n >>= 1;
+            n >>= 1; 
         }
         return result;
     }
