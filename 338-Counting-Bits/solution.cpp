ab813884ca10a29a969cf2444e252691e26ccbe8
xuchaoUCAS <xuchao0813@gmail.com>
2016-05-16 22:08:42 +0800

    [338. Counting Bits][Accepted]committed by LeetCode xuchao@ucas

--- 338-Counting-Bits/solution.cpp
+++ 338-Counting-Bits/solution.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> countBits(int num) {
         vector<int> result(num + 1, 0);
-        int offset = 1;
         for(int index = 1;index < num + 1; ++index){
             result[index] = result[index & (index - 1)] + 1;
         }
