540842d82265d4d8ffadae3838ce99b284fd4750
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-03 15:47:14 +0800

    [345. Reverse Vowels of a String][Accepted]committed by LeetCode xuchao@ucas

---
+++ 345-Reverse-Vowels-of-a-String/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isVowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            return true;
+        return false;
+    }
+    string reverseVowels(string s) {
+        int start = 0, end = s.length() - 1;
+        while(start < end){
+            while(start < end && !isVowel(s[start]))
+                ++start;
+            while(start < end && !isVowel(s[end]))
+                --end;
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+        return s;
+    }
+};
