e8307e1a60765b04bd73753d292e233dbdad69b1
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-14 11:27:08 +0800

    [113. Path Sum II][Accepted]committed by LeetCode xuchao@ucas

---
+++ 113-Path-Sum-II/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        if(!root)
+            return result;
+        help(root, sum, temp, result);
+        return result;
+    }
+    void help(TreeNode* root, int sum, vector<int> temp, vector<vector<int>>& result){
+        if(!root)
+            return;
+        if(!root->left && !root->right){
+            if(root->val == sum){
+                temp.push_back(root->val);
+                result.push_back(temp);
+            }
+        }
+        if(!root->left && root->right){
+            temp.push_back(root->val);
+            help(root->right, sum - root->val, temp, result);
+        }
+        else if(root->left && !root->right){
+            temp.push_back(root->val);
+            help(root->left, sum - root->val, temp, result);
+        }
+        else{
+            temp.push_back(root->val);
+            help(root->right, sum - root->val, temp, result);
+            help(root->left, sum - root->val, temp, result);
+        }
+    }
+};
