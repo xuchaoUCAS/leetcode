400f42a5713edee59bc1730bd75c816368423c24
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-14 22:07:12 +0800

    [20. Valid Parentheses][Accepted]committed by LeetCode xuchao@ucas

---
+++ 20-Valid-Parentheses/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto c : s){
+            switch(c){
+            case '(':
+            case '[':
+            case '{':
+                st.push(c);
+                break;
+            case ')':
+                if(st.empty() || st.top() != '(')
+                    return false;
+                else
+                    st.pop();
+                break;
+            case ']':
+                if(st.empty() || st.top() != '[')
+                    return false;
+                else
+                    st.pop();
+                break;
+            case '}':
+                if(st.empty() || st.top() != '{')
+                    return false;
+                else
+                    st.pop();
+                break;
+            }
+        }
+        return st.empty();
+    }
+};
