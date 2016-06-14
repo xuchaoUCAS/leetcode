19d6e48aab98817a0fbd474fed30733c914983e2
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-15 00:18:23 +0800

    [67. Add Binary][Accepted]committed by LeetCode xuchao@ucas

--- 67-Add-Binary/solution.cpp
+++ 67-Add-Binary/solution.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    string addBinary(string a, string b)
-    {
-        string s = "";
-
-        int c = 0, i = a.size() - 1, j = b.size() - 1;
-        while(i >= 0 || j >= 0 || c == 1)
-        {
-            c += i >= 0 ? a[i --] - '0' : 0;
-            c += j >= 0 ? b[j --] - '0' : 0;
-            s = char(c % 2 + '0') + s;
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.length() - 1, j = b.length() - 1, c = 0;
+        while(i >= 0 || j >= 0 || c == 1){
+            c += i >= 0 ? a[i--] - '0' : 0;
+            c += j >= 0 ? b[j--] - '0' : 0;
+            result = char(c % 2 + '0') + result;
             c /= 2;
         }
-
-        return s;
+        return result;
     }
 };
