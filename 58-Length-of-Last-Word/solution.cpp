e5802c15c3df88beefc80b23b44055d3b184b3f6
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-14 23:44:44 +0800

    [58. Length of Last Word][Accepted]committed by LeetCode xuchao@ucas

--- 58-Length-of-Last-Word/solution.cpp
+++ 58-Length-of-Last-Word/solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int len = 0, tail = s.length() - 1;
-        while (tail >= 0 && s[tail] == ' ') tail--;
-        while (tail >= 0 && s[tail] != ' ') {
-            len++;
-            tail--;
+        int len = 0, end = s.length() - 1;
+        while(end >= 0 && s[end] == ' ')
+            --end;
+        while(end >= 0 && s[end] != ' '){
+            --end;
+            ++len;
         }
         return len;
     }
