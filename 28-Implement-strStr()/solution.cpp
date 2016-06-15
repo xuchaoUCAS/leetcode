e77d5b0cd57db83b3c691a908a1a3b60848df3e9
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-15 09:54:23 +0800

    [28. Implement strStr()][Accepted]committed by LeetCode xuchao@ucas

--- 28-Implement-strStr()/solution.cpp
+++ 28-Implement-strStr()/solution.cpp
@@ -4,16 +4,15 @@ public:
         int m = haystack.length(), n = needle.length(), count_same = 0;
         if((!m && !n) || !n)
             return 0;
-        int i = 0,j = 0;
-        while(i < m){
-            count_same++;
-            if(haystack[i++] != needle[j++]){
-                j = 0;
-                i = i - count_same + 1;
-                count_same = 0;
+        for(int i = 0;i < m - n + 1;++i){
+            int j = 0;
+            while(j < n){
+                if(haystack[i + j] != needle[j])
+                    break;
+                j++;
             }
             if(j == n)
-                return i - count_same;
+                return i;
         }
         return -1;
     }
