dbfc56d003586b023a3c3991ef64d9334798102e
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-15 23:35:14 +0800

    [224. Basic Calculator][Accepted]committed by LeetCode xuchao@ucas

---
+++ 224-Basic-Calculator/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> nums, ops;
+        int num = 0;
+        int sign = 1;
+        int result = 0;
+        for(auto c : s){
+            if(isdigit(c))
+                num = num * 10 + c - '0';
+            else{
+                result += num * sign;
+                num = 0;
+                if(c == '+')
+                    sign = 1;
+                if(c == '-')
+                    sign = -1;
+                if(c == '('){
+                    nums.push(result);
+                    ops.push(sign);
+                    result = 0;
+                    num = 0;
+                    sign = 1;
+                }
+                if(c == ')' && ops.size()){
+                    result = ops.top() * result + nums.top();
+                    ops.pop();
+                    nums.pop();
+                }
+            }
+        }
+        result += sign *num;
+        return result;
+    }
+};
