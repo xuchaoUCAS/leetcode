d5a757efc213413e8e1f59a132a72e70e314252b
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-18 01:06:34 +0800

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode xuchao@ucas

---
+++ 5-Longest-Palindromic-Substring/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int len = s.length();
+        if(!len)
+            return s;
+        string result;
+        int maxLen = 0;
+        for(int i = 0;i < s.length();++i){
+            int pos = 0;
+            while((i - pos >= 0) && (i + pos < len) && s[i - pos] == s[i + pos])
+                ++pos;
+            --pos;
+            if(maxLen < 2 * pos + 1){
+                maxLen = 2 * pos + 1;
+                result = s.substr(i - pos, 2 * pos + 1);
+            }
+           
+            
+            pos = 0;
+            while((i - pos >= 0) && (i + pos + 1 < len) && s[i - pos] == s[i + pos + 1])
+                ++pos;
+            --pos;
+            if(maxLen < 2 * pos + 2){
+                maxLen = 2 * pos + 2;
+                result = s.substr(i - pos, 2 * pos + 2);
+            }
+        }
+        return result;
+    }
+};
