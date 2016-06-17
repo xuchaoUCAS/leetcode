5460f12c39d5f98160a64612b838d714a3f528f1
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-17 22:53:31 +0800

    [300. Longest Increasing Subsequence][Accepted]committed by LeetCode xuchao@ucas

---
+++ 300-Longest-Increasing-Subsequence/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int len = nums.size();
+        if(!len)
+            return 0;
+        vector<int> dp = vector<int>(len, 0);
+        dp[0] = 1;
+        int maxd = -1, result = dp[0];
+        for(int i = 1;i < len;++i){
+            for(int j = 0;j < i;++j){
+                if(nums[j] < nums[i]){
+                    maxd = max(maxd, dp[j]);
+                }
+            }
+            dp[i] = maxd > 0 ? maxd + 1 : 1;
+            result = max(result, dp[i]);
+            maxd = -1;
+        }
+        return result;
+    }
+};
