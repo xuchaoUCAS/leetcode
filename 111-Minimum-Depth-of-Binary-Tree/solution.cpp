a1a885e2a97a589f9fc7118f979ae7b8556004eb
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-19 01:43:09 +0800

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode xuchao@ucas

--- 111-Minimum-Depth-of-Binary-Tree/solution.cpp
+++ 111-Minimum-Depth-of-Binary-Tree/solution.cpp
@@ -15,8 +15,8 @@ public:
     int minDepth(TreeNode* root) {
         if(!root)
             return 0;
-         if(!root->left) return 1 + minDepth(root->right);
-         if(!root->right) return 1 + minDepth(root->left);
+         if(!root->left && root->right) return 1 + minDepth(root->right);
+         if(!root->right && root->left) return 1 + minDepth(root->left);
         return 1 + min(minDepth(root->left), minDepth(root->right));
     }
 };
