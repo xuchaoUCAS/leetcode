904d14b66ec0deeef09c9cbe20b152a669776a18
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-19 13:30:37 +0800

    [100. Same Tree][Accepted]committed by LeetCode xuchao@ucas

--- 100-Same-Tree/solution.cpp
+++ 100-Same-Tree/solution.cpp
@@ -10,9 +10,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q)
+        if(!q && !p)
             return true;
-        if((!p && q) || (p && !q))
+        if(!q || !p)
             return false;
         if(p->val == q->val)
             return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
