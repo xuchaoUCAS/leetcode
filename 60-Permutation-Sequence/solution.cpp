fb4103f2f0656c05b4002f64ade90d205d16737a
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-20 22:59:31 +0800

    [60. Permutation Sequence][Accepted]committed by LeetCode xuchao@ucas

---
+++ 60-Permutation-Sequence/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fac[9] = {1};
+        for(int i = 1;i <= 9;++i){
+            fac[i] = fac[i - 1] * i; 
+        }
+        
+        string num = "123456789";
+        string result;
+        while(n > 0){
+            int temp = (k - 1) / fac[n - 1];
+            result += num[temp];
+            num.erase(num.begin() + temp);
+            k = k - temp * fac[n - 1];
+            n--;
+        }
+        return result;
+    }
+};
