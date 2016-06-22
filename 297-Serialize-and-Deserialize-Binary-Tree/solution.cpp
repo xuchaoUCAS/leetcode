889a3648e0c29224cad0da1513f54c860e1f3bb0
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-22 13:21:23 +0800

    [297. Serialize and Deserialize Binary Tree][Accepted]committed by LeetCode xuchao@ucas

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.cpp
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.cpp
@@ -8,40 +8,66 @@
  * };
  */
 class Codec {
-    void serialize(TreeNode * root, stringstream &ss){
-        if(!root){
-            ss<<"# ";
-        }else{
-            ss<<root->val<<" ";
-            serialize(root->left, ss);
-            serialize(root->right, ss);
-        }
-    }
-    TreeNode *deserialize(stringstream &ss){
-        string s;
-        ss>>s;
-        if(s=="#"){
-            return NULL;
-        }else{
-            TreeNode *node = new TreeNode(stoi(s));
-            node->left = deserialize(ss);
-            node->right = deserialize(ss);
-            return node;
-        }
-    }
 public:
- 
+
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         stringstream ss;
-        serialize(root, ss);
+        ser(root, ss);
         return ss.str();
     }
- 
+
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
         stringstream ss(data);
-        return deserialize(ss);
+        string s;
+        ss >> s;
+        if(s == "#")
+            return NULL;
+        TreeNode* root = new TreeNode(stoi(s));
+        return deserialize(ss, root);
+    }
+private:
+    void ser(TreeNode* root, stringstream& ss){
+        queue<TreeNode*> q;
+        q.push(root);
+        if(!root){
+            ss << "# ";
+            return;
+        }
+        else
+            ss << root->val << " ";
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            if(temp){
+                q.push(temp->left);
+                q.push(temp->right);
+                temp->left ? ss << temp->left->val << " " : ss << "# ";
+                temp->right ? ss << temp->right->val << " " : ss << "# "; 
+            }
+            q.pop();
+        }
+    }
+    
+    TreeNode* deserialize(stringstream& ss, TreeNode* root){
+        queue<TreeNode*> q;
+        string left,right;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            if(temp){
+                ss >> left;
+                ss >> right;
+                left == "#" ? temp->left = NULL : temp->left = new TreeNode(stoi(left));
+                
+                
+                right == "#" ? temp->right = NULL : temp->right = new TreeNode(stoi(right));
+                q.push(temp->left);
+                q.push(temp->right);
+            }
+            q.pop();
+        }
+        return root;
     }
 };
 
