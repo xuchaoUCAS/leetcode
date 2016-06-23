3fd1fbc6d0dcf6b6d86552f88db40cf52a150b27
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-23 21:10:12 +0800

    [14. Longest Common Prefix][Accepted]committed by LeetCode xuchao@ucas

--- 14-Longest-Common-Prefix/solution.cpp
+++ 14-Longest-Common-Prefix/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string result ;
         int index = 0;
+        string prefix;
         while(strs.size()){
-            for(int i = 0;i < strs.size();++i)
+            for(int i = 0;i < strs.size();++i){
                 if(index >= strs[i].size() || (i > 0 && strs[i][index] != strs[i - 1][index]))
-                    return result;
-            result += strs[0][index];
-            ++index;
+                    return prefix;
+            }
+            prefix += strs[0][index];
+            index++;
         }
-        return result;
+        return prefix;
     }
 };
