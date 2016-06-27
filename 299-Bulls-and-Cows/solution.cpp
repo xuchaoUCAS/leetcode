2f0cd6eb3e0a6e6f125ed1bef9abf03f71684179
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-27 21:26:56 +0800

    [299. Bulls and Cows][Accepted]committed by LeetCode xuchao@ucas

--- 299-Bulls-and-Cows/solution.cpp
+++ 299-Bulls-and-Cows/solution.cpp
@@ -3,19 +3,19 @@ public:
     string getHint(string secret, string guess) {
         int len = min(secret.length(), guess.length());
         int countA = 0, countB = 0;
-        int tableA[10] = {0}, tableB[10] = {0};
+        int tableA[10] = {0};
         for(int i = 0;i < len;++i){
             if(secret[i] == guess[i])
                 ++countA;
             else{
+                if(tableA[secret[i] - '0'] < 0)
+                    ++countB;
+                if(tableA[guess[i] - '0'] > 0)
+                    ++countB;
                 ++tableA[secret[i] - '0'];
-                ++tableB[guess[i] - '0'];
+                --tableA[guess[i] - '0'];
             }
         }
-        for(int i = 0;i < 10;++i){
-            countB += min(tableA[i], tableB[i]);
-        }
-        
         return to_string(countA) + "A" + to_string(countB) + "B";
     }
 };
