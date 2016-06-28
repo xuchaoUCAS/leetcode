d84bca83e4c1d37b9b147a3b9b1c9eed383afd28
xuchaoUCAS <xuchao0813@gmail.com>
2016-06-28 23:31:34 +0800

    [88. Merge Sorted Array][Accepted]committed by LeetCode xuchao@ucas

--- 88-Merge-Sorted-Array/solution.cpp
+++ 88-Merge-Sorted-Array/solution.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        nums1.resize(m + n);
-        int len = m + n;
-        while(m || n){
-            if(m <= 0){
-                nums1[len - 1] = nums2[n - 1];
-                --n;
-            }
-            else if(n <= 0){
-                nums1[len - 1] = nums1[m - 1];
-                --m;
-            }
-            else if(nums1[m - 1] > nums2[n - 1] || n <= 0){
-                nums1[len - 1] = nums1[m - 1];
-                --m;
-            }
-            else{
-                nums1[len - 1] = nums2[n - 1];
-                --n;
-            }
-            --len;
+        int i = m - 1;
+        int j = n - 1;
+        int len = m + n - 1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j])
+                nums1[len--] = nums1[i--];
+            else
+                nums1[len--] = nums2[j--];
         }
+        while(j >= 0)
+            nums1[len--] = nums2[j--];
     }
 };
