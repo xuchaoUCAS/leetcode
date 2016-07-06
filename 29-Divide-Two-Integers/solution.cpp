7e25d6f6e4935e3b60ddf7b798f76e74af868f2d
xuchaoUCAS <xuchao0813@gmail.com>
2016-07-06 21:01:05 +0800

    [29. Divide Two Integers][Accepted]committed by LeetCode xuchao@ucas

--- 29-Divide-Two-Integers/solution.cpp
+++ 29-Divide-Two-Integers/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (!divisor || (dividend == INT_MIN && divisor == -1))
+        if(!divisor || (dividend == INT_MIN && divisor == -1))
             return INT_MAX;
+        int result = 0;
         int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
-        long long dvd = labs(dividend);
-        long long dvs = labs(divisor);
-        int res = 0;
-        while (dvd >= dvs) { 
-            long long temp = dvs, multiple = 1;
-            while (dvd >= (temp << 1)) {
+        long long din = labs(dividend);
+        long long dis = labs(divisor);
+        
+        while(din >= dis){
+            long long temp = dis, mul = 1;
+            while(din >= (temp << 1)){
                 temp <<= 1;
-                multiple <<= 1;
+                mul <<= 1;
             }
-            dvd -= temp;
-            res += multiple;
+            din -= temp;
+            result += mul;
         }
-        return sign == 1 ? res : -res; 
+        return sign == 1 ? result : -result; 
     }
 };
