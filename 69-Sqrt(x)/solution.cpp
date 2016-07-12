e4c818cde180e800019c9202d8e16b73202ee9aa
xuchaoUCAS <xuchao0813@gmail.com>
2016-07-12 17:12:57 +0800

    [69. Sqrt(x)][Accepted]committed by LeetCode xuchao@ucas

--- 69-Sqrt(x)/solution.cpp
+++ 69-Sqrt(x)/solution.cpp
@@ -9,7 +9,7 @@ public:
             if(mid > x / mid)
                 r = mid - 1;
             else{
-                if(mid + 1 > x / (mid + 1))
+                if (mid + 1 > x/(mid + 1))
                     return mid;
                 l = mid + 1;
             }
