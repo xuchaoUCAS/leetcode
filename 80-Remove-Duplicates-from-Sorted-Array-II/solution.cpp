66d652733fe89751645affefbde636f245e833db
xuchaoUCAS <xuchao0813@gmail.com>
2016-07-16 15:06:07 +0800

    [80. Remove Duplicates from Sorted Array II][Accepted]committed by LeetCode xuchao@ucas

--- 80-Remove-Duplicates-from-Sorted-Array-II/solution.cpp
+++ 80-Remove-Duplicates-from-Sorted-Array-II/solution.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int length = nums.size();
         int newLength = 0;
-        int dup = 1;
-        for(int i = 0;i < length;++i){
-            if(i > 0 && nums[i] == nums[i - 1])
-                dup++;
-            if(i > 0 && nums[i] != nums[i - 1])
-                dup = 1;
-            if(i == 0 || dup <= 2)
-                nums[newLength++] = nums[i];
+        for(auto n : nums){
+            if(newLength < 2 || n > nums[newLength - 2])
+                nums[newLength++] = n;
         }
         return newLength;
+        
     }
 };
