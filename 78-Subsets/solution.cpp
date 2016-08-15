71473622b2a47eefb47732273294557f20ccea59
xuchaoUCAS <xuchao0813@gmail.com>
2016-08-15 12:23:42 +0800

    [78. Subsets][Accepted]committed by LeetCode xuchao@ucas

--- 78-Subsets/solution.cpp
+++ 78-Subsets/solution.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int len = nums.size();
-        int total = pow(2, len);
-        vector<vector<int>> result(total, vector<int>());
-        for(int i = 0;i < len;++i)
-            for(int j = 0;j < total;++j){
-                if((j >> i) & 1)
-                    result[j].push_back(nums[i]);
-            }
-        return result;
+        vector<vector<int>> subs;
+        vector<int> sub;  
+        genSubsets(nums, 0, sub, subs);
+        return subs; 
+    }
+    void genSubsets(vector<int>& nums, int start, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int i = start; i < nums.size(); i++) {
+            sub.push_back(nums[i]);
+            genSubsets(nums, i + 1, sub, subs);
+            sub.pop_back();
+        }
     }
 };
