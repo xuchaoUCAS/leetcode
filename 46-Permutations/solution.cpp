3314499a352d5b6f8e47b2463c684ad5dc9000cf
xuchaoUCAS <xuchao0813@gmail.com>
2016-09-02 12:31:37 +0800

    [46. Permutations][Accepted]committed by LeetCode xuchao@ucas

--- 46-Permutations/solution.cpp
+++ 46-Permutations/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    void per(vector<int> s,int start,vector<vector<int>>& result){
+    void per(vector<int>& s,int start,vector<vector<int>>& result){
         if(start == s.size())
             result.push_back(s);
         else{
             for(int i = start;i < s.size();++i){
                 swap(s[start], s[i]);
                 per(s, start + 1, result);
-                //swap(s[start], s[i]);
+                swap(s[start], s[i]);
             }
         }
     }
