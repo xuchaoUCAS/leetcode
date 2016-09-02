a03b8882f341eb430b1eec57a2dc2ca1fef95d85
xuchaoUCAS <xuchao0813@gmail.com>
2016-09-02 11:27:59 +0800

    [47. Permutations II][Accepted]committed by LeetCode xuchao@ucas

--- 47-Permutations-II/solution.cpp
+++ 47-Permutations-II/solution.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    bool hasSwap(vector<int>s, int start, int end){
-        while(start < end){
-            if(s[start] == s[end])
-                return false;
-            start++;
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
         }
-        return true;
-    }
-
-
-    void per(vector<int> s, int start, vector<vector<int>>& result){
-        if(start == s.size())
-            result.push_back(s);
-        else{
-            for(int i = start;i < s.size();++i){
-                if(hasSwap(s, start, i)){
-                    swap(s[start], s[i]);
-                    per(s, start + 1, result);
-                    swap(s[start], s[i]);
-                }
-            }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
         }
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> result;
-        per(nums, 0, result);
-        return result;
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
     }
 };
